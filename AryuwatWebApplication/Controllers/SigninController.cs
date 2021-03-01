da57cbb0f4945f0dbdf16139fbea74eb4d2b4671
Parinya Apaipak <parinya.ekk@gmail.com>
2021-03-01 20:17:49 +0700

    Decode

--- AryuwatWebApplication/Controllers/SigninController.cs
+++ AryuwatWebApplication/Controllers/SigninController.cs
@@ -14,6 +14,17 @@ namespace AryuwatWebApplication.Controllers
         {
             return View();
         }
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
 
         [HttpPost]
         public ActionResult Login(string tempdata)
@@ -28,14 +39,14 @@ namespace AryuwatWebApplication.Controllers
                     if(chkLogin != null)
                     {
                         Response.Cookies["OPD"]["ID"] = chkLogin.ID.ToString();
-                        Response.Cookies["OPD"]["TName"] = chkLogin.TName == null ? null : chkLogin.TName.ToString();
-                        Response.Cookies["OPD"]["Username"] = chkLogin.Username == null ? null : chkLogin.Username.ToString();
+                        Response.Cookies["OPD"]["TName"] = chkLogin.TName == null ? null : Base64Encode(chkLogin.TName.ToString());
+                        Response.Cookies["OPD"]["Username"] = chkLogin.Username == null ? null : Base64Encode(chkLogin.Username.ToString());
                         Response.Cookies["OPD"]["IdCard"] = chkLogin.IdCard == null ? null : chkLogin.IdCard.ToString();
-                        Response.Cookies["OPD"]["PersonnelType"] = chkLogin.PersonnelType == null ? null : chkLogin.PersonnelType.ToString();
-                        Response.Cookies["OPD"]["UserGroup"] = chkLogin.UserGroup == null ? null : chkLogin.UserGroup.ToString();
-                        Response.Cookies["OPD"]["ImageFilename"] = chkLogin.ImageFilename == null ? null : chkLogin.ImageFilename.ToString();
+                        Response.Cookies["OPD"]["PersonnelType"] = chkLogin.PersonnelType == null ? null : Base64Encode(chkLogin.PersonnelType.ToString());
+                        Response.Cookies["OPD"]["UserGroup"] = chkLogin.UserGroup == null ? null : Base64Encode(chkLogin.UserGroup.ToString());
+                        Response.Cookies["OPD"]["ImageFilename"] = chkLogin.ImageFilename == null ? null : Base64Encode(chkLogin.ImageFilename.ToString());
                         Response.Cookies["OPD"].Expires = DateTime.Now.AddMinutes(180);
-                        return Json(new { status = 200 });
+                        return Json(new { status = 200, data = Response.Cookies["OPD"] });
                     }
                     return Json(new { status = 400 });
                 }
