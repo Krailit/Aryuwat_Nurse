c60ec358d68e762a07e75cd4c2380dcd8c128f1e
parinya.ekk@gmail.com <parinya.ekk@gmail.com>
2021-05-11 21:01:14 +0700

    test

--- AryuwatSystem/Forms/FrmMedicalOrderSettingPro.cs
+++ AryuwatSystem/Forms/FrmMedicalOrderSettingPro.cs
@@ -4929,7 +4929,7 @@ namespace AryuwatSystem.Forms
                             using (var context = new EntitiesOPD_System())
                             {
                                 var getRoomMO = context.MedicalOrders.Where(x => x.Room_Status == true).Take(1).OrderByDescending(x => x.ID).FirstOrDefault();
-                                int maxid = getRoomMO != null ? Convert.ToInt32(getRoomMO.SONo.Substring(12, 4).ToString()) : 0;
+                                int maxid = getRoomMO != null ? !String.IsNullOrEmpty(getRoomMO.SONo) ? Convert.ToInt32(getRoomMO.SONo.Substring(12, 4).ToString()) : 0 : 0;
                                 var strCheck = moso + MoSubType + "-" + ((DateTime.Now.Year + 543).ToString().Substring(2, 2) + (DateTime.Now.Month).ToString("0#") + (maxid + 1).ToString("000#")).ToString();
                                 this.idMax = strCheck;
                             }
@@ -6277,7 +6277,8 @@ namespace AryuwatSystem.Forms
                         using (var context = new EntitiesOPD_System())
                         {
                             var getRoomMO = context.MedicalOrders.Where(x => x.Room_Status == true).Take(1).OrderByDescending(x => x.ID).FirstOrDefault();
-                            int maxid = getRoomMO != null ? Convert.ToInt32(getRoomMO.SONo.Substring(12, 4).ToString()) : 0; var strCheck = moso + MoSubType + "-" + ((DateTime.Now.Year + 543).ToString().Substring(2, 2) + (DateTime.Now.Month).ToString("0#") + (maxid + 1).ToString("000#")).ToString();
+                            int maxid = getRoomMO != null ? !String.IsNullOrEmpty(getRoomMO.SONo) ? Convert.ToInt32(getRoomMO.SONo.Substring(12, 4).ToString()) : 0 : 0; 
+                            var strCheck = moso + MoSubType + "-" + ((DateTime.Now.Year + 543).ToString().Substring(2, 2) + (DateTime.Now.Month).ToString("0#") + (maxid + 1).ToString("000#")).ToString();
                             this.idMax = strCheck;
                         }
                     }
